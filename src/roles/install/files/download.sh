906847330fb95138839f25e4ae8fa815ecf4bb45
yibo.tang <yibo.tang@transwarp.io>
2022-01-29 13:10:20 +0800

    update greenplum to latest version 6.19.1

---
+++ src/roles/install/files/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+VERSION="6.19.1"
+
+wget --no-check-certificate https://github.com.cnpmjs.org/greenplum-db/gpdb/releases/download/$VERSION/open-source-greenplum-db-$VERSION-rhel7-x86_64.rpm
